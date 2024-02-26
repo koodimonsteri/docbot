ef102cf540e3dab03f1c8d75772cbd3f77ad1723
unknown <otto.vonboehm@aalto.fi>
2024-02-26 23:57:44 +0200

    Stream llm responses to frontend

--- backend/app/routers/chat.py
+++ backend/app/routers/chat.py
@@ -1,5 +1,5 @@
 import json
-from typing import Dict
+from typing import Dict, List
 
 from fastapi import APIRouter, FastAPI, WebSocket, WebSocketDisconnect, Request, Depends, HTTPException
 from fastapi.responses import HTMLResponse
@@ -15,42 +15,72 @@ router = APIRouter(
 class WebSocketManager:
     def __init__(self):
         self.connections: Dict[str, WebSocket] = {}
+        self.chat_histories: Dict[str, List[Dict]] = {}
 
     def add_connection(self, user_id: str, websocket: WebSocket):
         self.connections[user_id] = websocket
+        self.chat_histories[user_id] = []
 
     def remove_connection(self, user_id: str):
         if user_id in self.connections:
             del self.connections[user_id]
+        if user_id in self.chat_histories:
+            del self.chat_histories[user_id]
 
-    async def send_message(self, user_id: str, message: str):
+    def add_message(self, user_id, message: Dict):
+        print('Add message to chat history:', message)
+        if user_id in self.chat_histories:
+            self.chat_histories[user_id].append(message)
+        else:
+            self.chat_histories[user_id] = [message]
+
+    async def send_message(self, user_id: str, message: Dict):
         if user_id in self.connections:
-            await self.connections[user_id].send_text(message)
+            await self.connections[user_id].send_json(message)
+        if user_id in self.chat_histories:
+            self.chat_histories.append
+
 
 manager = WebSocketManager()
-
 
 @router.websocket("/ws/{user_id}")
 async def chat_endpoint(user_id: str, websocket: WebSocket):
     print('Opening websocket connection')
     await websocket.accept()
     manager.add_connection(user_id, websocket)
+    message_id = 1
     try:
         while True:
             message_json = await websocket.receive_json()
             message_text = message_json.get('text')
             message_filename = message_json.get('file_name')
-            
+            message_id = message_json.get('id')
+            manager.add_message(user_id, {'type': 'user', 'id': message_id, 'text': message_text})
+            message_id += 1
+
             if not message_filename:
-                response = await get_openai_response(message_text)
-                print('Sending response back to client:', response)
+                full_text = ""
+                async for partial_response in get_openai_response_stream(message_text):
+                    if partial_response is not None:
+                        full_text += partial_response
+                        await websocket.send_json({'text': partial_response, 'id': message_id, 'type': 'bot'})
+                manager.add_message(user_id, {'type': 'bot', 'id': message_id, 'text': full_text})
+                message_id += 1
             else:
-                # Embeddings to chat in here
-                response = await get_openai_response_from_file(message_filename, message_text)
-                
-            await manager.send_message(user_id, response)
+                full_text = ""
+                async for partial_response in get_openai_response_from_file_stream(message_filename, message_text):
+                    if partial_response is not None:
+                        full_text += partial_response
+                        await websocket.send_json({'text': partial_response, 'id': message_id, 'type': 'bot'})
+
+                manager.add_message(user_id, {'type': 'bot', 'id': message_id, 'text': full_text})
+                message_id += 1
+
     except WebSocketDisconnect:
         print('Websocket disconnected:', user_id)
+        manager.remove_connection(user_id)
+    except Exception as e:
+        print('Exception:', e)
         manager.remove_connection(user_id)
 
 
@@ -65,6 +95,17 @@ async def get_openai_response(message: str) -> str:
     return res[0]
 
 
+async def get_openai_response_stream(message: str):
+    client = OpenAI()
+    for partial_result in client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": message}],
+        stream=True
+    ):
+        partial_msg = partial_result.choices[0].delta.content
+        yield partial_msg
+
+
 async def get_openai_response_from_file(file_name: str, message: str) -> str:
     embeddings_result = search_embeddings(file_name, message)
     print(embeddings_result)
@@ -76,5 +117,18 @@ async def get_openai_response_from_file(file_name: str, message: str) -> str:
     )
 
     res = [x.message.content.strip() for x in result.choices]
-    print(res)
     return res[0]
+
+
+async def get_openai_response_from_file_stream(file_name: str, message: str):
+    embeddings_result = search_embeddings(file_name, message)
+    print(embeddings_result)
+
+    client = OpenAI()
+    for partial_result in client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": message}] + [{"role": "assistant", "content": response} for response in embeddings_result],
+        stream=True
+    ):
+        partial_msg = partial_result.choices[0].delta.content
+        yield partial_msg
--- frontend/app/src/ChatBot.js
+++ frontend/app/src/ChatBot.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import './ChatBot.css';
 
+
 const ChatBot = () => {
   const [chatHistory, setChatHistory] = useState([]);
   const [newMessage, setNewMessage] = useState('');
@@ -11,21 +12,51 @@ const ChatBot = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [uploadFile, setUploadFile] = useState(null)
   const [uploadStatus, setUploadStatus] = useState(null);
+  const [messages, setMessages] = useState([])
+
+  const generateMessageId = (() => {
+    let messageId = 0;
+  
+    return (messages) => {
+      if (!Array.isArray(messages) || messages.length === 0) {
+        messageId += 1;
+      } else {
+        const maxId = messages.reduce((max, message) => (message.id > max ? message.id : max), 0);
+        messageId = maxId + 1;
+      }
+  
+      return messageId;
+    };
+  })();
 
   useEffect(() => {
     const generatedUserId = uuidv4();
     setUserId(generatedUserId);
 
     const ws = new WebSocket(`ws://localhost:8000/chat/ws/${generatedUserId}`);
-    
+
     ws.onopen = () => {
       console.log('WebSocket connection opened');
     };
 
     ws.onmessage = event => {
-      const message = event.data
-      console.log('Received message:', message)
-      updateChatHistory({ text: event.data, type: 'received' });
+      const partialResponse = JSON.parse(event.data);
+      console.log('partial response:', partialResponse)
+      setMessages((prevMessages) => {
+        const existingMessageIndex = prevMessages.findIndex((msg) => msg && msg.id === partialResponse.id);
+        if (existingMessageIndex !== -1) {
+          return prevMessages.map((msg) =>
+            msg.id === partialResponse.id
+              ? {
+                  ...msg,
+                  text: `${msg.text || ''}${partialResponse.text ? `${partialResponse.text}` : ''}`,
+                }
+              : msg
+          );
+        } else {
+          return [...prevMessages, partialResponse];
+      }
+    });
     };
 
     ws.onclose = () => {
@@ -35,7 +66,6 @@ const ChatBot = () => {
     setWebsocket(ws);
 
     fetchExistingFiles();
-
     return () => {
       ws.close();
     };
@@ -93,21 +123,21 @@ const handleDropDownFileSelect = (e) => {
     setSelectedFile(selected);
   };
 
-  const updateChatHistory = (message) => {
-    setChatHistory(prevHistory => [...prevHistory, message]);
-  };
-
   const handleSendMessage = () => {
     console.log('Attempting to send message');
     console.log('websocket:', websocket);
     console.log('newMessage:', newMessage);
-
+    const messageId = generateMessageId(messages);
     if (websocket && newMessage.trim() !== '') {
-      const message = { text: newMessage, type: 'sent' };
-      console.log('Sending message:', message);
-
-      updateChatHistory(message);
-      websocket.send(JSON.stringify({ text: newMessage, file_name: selectedFile?.name || ''}));
+      const messageItem = {
+        id: messageId,
+        text: newMessage,
+        type: 'user',
+        file_name: selectedFile?.name || '',
+      };
+      console.log('Sending message:', messageItem);
+      setMessages(prevHistory => [...prevHistory, messageItem]);
+      websocket.send(JSON.stringify(messageItem));
       setNewMessage('');
     }
   };
@@ -144,14 +174,14 @@ const handleDropDownFileSelect = (e) => {
         <div className="chat-container">
             <div className="chat-history">
                 <ul>
-                {chatHistory.map((entry, index) => (
-                    <li key={index} className={`message ${entry.type}`}>
-                    {entry.type === 'sent' ? (
+                {messages.map((entry) => (
+                    <li key={entry.id} className={`message ${entry.type}`}>
+                    {entry.type === 'user' ? (
                         <span className="indicator">User</span>
                     ) : (
                         <span className="indicator">Bot</span>
                     )}
-                    {entry.text}
+                    {entry.text !== undefined ? entry.text : ''}
                     </li>
                 ))}
                 </ul>
